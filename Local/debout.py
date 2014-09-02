e1dc2a594041560db8f2ebb61916d03d9a3941ca
rouviecy <rouvcyril@yahoo.fr>
2014-09-02 17:48:53 +0200

    Added postures

---
+++ Local/debout.py
@@ -0,0 +1,8 @@
+from naoqi import ALProxy
+import sys, os
+
+if __name__ == "__main__":
+	IP, PORT = "127.0.0.1", 9559
+	os.system("python stiffness.py 1")
+	postureProxy = ALProxy("ALRobotPosture", IP, PORT)
+	postureProxy.goToPosture("Stand", 1.0)
