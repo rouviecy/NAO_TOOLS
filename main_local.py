b6cc48e2ebd0e204613bf6d9ae729c6ab349818d
rouviecy <rouvcyril@yahoo.fr>
2014-09-10 20:25:42 +0200

    Depreciated : no GUI on NAO !

--- main_local.py
+++
@@ -1,8 +0,0 @@
-from GUI				import GUI
-from Interface_serveur	import Interface_serveur
-from Interface_client	import Interface_client
-from Actionneur			import Actionneur
-
-if __name__ == '__main__':
-	actionneur = Actionneur()
-	gui = GUI(actionneur)
